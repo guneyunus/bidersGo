11654e7cdd5a3bc18b1e8b51f697f85910fff46f
hkpln <h.kpln0681@gmail.com>
2022-02-04 12:22:57 +0300

    theme added

---
+++ bidersGo/wwwroot/assets/js/pages/session-timeout.init.js
@@ -0,0 +1,5 @@
+//$.sessionTimeout({ keepAliveUrl: "pages-starter.html", logoutButton: "Logout", logoutUrl: "auth-login.html", redirUrl: "auth-lock-screen.html", warnAfter: 3e3, redirAfter: 3e4, countdownMessage: "Redirecting in {timer} seconds." });
+
+
+$.sessionTimeout({ keepAliveUrl: "/", logoutButton: "Logout", logoutUrl: "AuthLogin", redirUrl: "/", warnAfter: 3e3, redirAfter: 3e4, countdownMessage: "Redirecting in {timer} seconds." });
+
