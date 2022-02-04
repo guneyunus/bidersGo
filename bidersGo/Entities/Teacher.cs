8e579af10c60b3735947cdc81573a50fb32c366b
guneyunus <guney.yunus.dev@outlook.com>
2022-02-04 11:05:26 +0300

    entites first

---
+++ bidersGo/Entities/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bidersGo.Entities
+{
+    public class Teacher
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+
+    }
+}
