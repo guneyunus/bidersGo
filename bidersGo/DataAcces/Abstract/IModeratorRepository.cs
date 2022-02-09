f8043ba49cf9f32e024e625e4e80ac7a5c3b3621
guneyunus <guney.yunus.dev@outlook.com>
2022-02-09 11:18:22 +0300

    Update Repository

--- bidersGo/DataAcces/Abstract/IModeratorRepository.cs
+++ bidersGo/DataAcces/Abstract/IModeratorRepository.cs
@@ -8,5 +8,31 @@ namespace bidersGo.DataAcces.Abstract
 {
     public interface IModeratorRepository : IRepository<Moderator>
     {
+        Student GetStudentById(Guid StudentGuid);
+        Student GetStudentByName(string Name);
+        List<Student> GetStudentsAll();
+        Address GetStudentAdress(Student Student);
+        List<Meet> GetStudentMeet(Student Student);
+        Teacher GetTeacherById(Guid TeacherGuid);
+        Teacher GetTeacherByName(string TeacherName);
+        List<Teacher> GetAllTeacher();
+        Address GetAddress(Teacher teacher);
+        List<Meet> GetTeacherMeet(Teacher teacher);
+
+        List<Meet> GetMeetNow();
+        Meet GetMeetById(Guid MeetGuid);
+        DateTime GetMeetDateTime(Meet Meet);
+        void CreateMeet(Student student,Teacher teacher,Meet meet);
+
+
+
+
+
+
+
+
+
+
+
     }
 }
