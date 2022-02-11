156acb5c51db04b9fe283350ba37e0dfcf6455d5
guneyunus <guney.yunus.dev@outlook.com>
2022-02-11 11:11:01 +0300

    businessapi

---
+++ bidersGo/BusinessApi/Concrate/GenericBusinessApi.cs
@@ -0,0 +1,33 @@
+﻿using bidersGo.Business.Abstract;
+using bidersGo.DataAcces.Abstract;
+using bidersGo.DataAcces.Conctare;
+using Microsoft.EntityFrameworkCore;
+
+namespace bidersGo.BusinessApi.Concrate
+{
+    public class GenericBusinessApi<Tentity> where Tentity : class
+    {
+        private readonly IAdminService _adminService;
+        private readonly IApplicationLogger _applicationLogger;
+        private readonly IModeratorService _moderatorService;
+        private readonly IStudentService _studentService;
+        private readonly ITeacherService _teacherService;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly BidersGoContext _context;
+
+        public GenericBusinessApi()
+        {
+            
+        }
+        public GenericBusinessApi(BidersGoContext context,IAdminService adminService,IApplicationLogger logger,IModeratorService moderatorService,IStudentService studentService,ITeacherService teacherService,IUnitOfWork unitOfWork)
+        {
+            _adminService = adminService;
+            _applicationLogger = logger;
+            _moderatorService = moderatorService;
+            _studentService = studentService;
+            _teacherService = teacherService;
+            _unitOfWork = unitOfWork;
+            _context = context;
+        }
+    }
+}
