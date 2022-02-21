b6445a9279cb25989408bfbd0adeab8fac4b0e33
hkpln <h.kpln0681@gmail.com>
2022-02-21 14:10:08 +0300

    :rocket:

---
+++ bidersGo.Application/Features/Queries/TeacherGetByLesson/TeacherGetByLessonQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using bidersGo.Application.Interfaces.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace bidersGo.Application.Features.Queries.TeacherGetByLesson
+{
+    public class TeacherGetByLessonQueryHandler : IRequestHandler<TeacherGetByLessonQueryRequest, TeacherGetByLessonQueryResponse>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public TeacherGetByLessonQueryHandler(IUnitOfWork unitOfWork,IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public Task<TeacherGetByLessonQueryResponse> Handle(TeacherGetByLessonQueryRequest request, CancellationToken cancellationToken)
+        {
+            var teacher = _unitOfWork.TeacherRepository.GetTeacherById(request.LessonId);
+
+            TeacherGetByLessonQueryResponse model = _mapper.Map<TeacherGetByLessonQueryResponse>(teacher);
+            return Task<TeacherGetByLessonQueryResponse>.FromResult(model);
+        }
+    }
+}
---
+++ bidersGo.Application/Features/Queries/TeacherGetByLesson/TeacherGetByLessonQueryResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bidersGo.Application.Features.Queries.TeacherGetByLesson
+{
+    public class TeacherGetByLessonQueryResponse
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Branch { get; set; }
+        public int TcKimlik { get; set; }
+        public string NickName { get; set; }
+        public string Email { get; set; }
+    }
+}
