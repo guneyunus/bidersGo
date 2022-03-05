5cf91f1019a96d136986752324ad88007b916e8b
Aydoğan Selçuk Kılıçtaş <92909567+Sphinxs77@users.noreply.github.com>
2022-03-05 13:40:50 +0300

    .

--- bidersGo.Application/Features/Queries/ModeratorGetById/ModeratorGetByIdQueryResponse.cs
+++ bidersGo.Application/Features/Queries/ModeratorGetById/ModeratorGetByIdQueryResponse.cs
@@ -8,11 +8,12 @@ namespace bidersGo.Application.Features.Queries.ModeratorGetById
 {
     public class ModeratorGetByIdQueryResponse
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string NickName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public int TcKimlik { get; set; }
+        public string TcKimlik { get; set; }
     }
 }
